c4a0de5265a4483caf43ae69550858dd95a75e90
Benedict <benedictumeozor@gmail.com>
2024-03-15 12:04:29 +0100

    container width

--- components/Container.tsx
+++ components/Container.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Container({ children, className }: Props) {
   return (
-    <div className={"max-w-5xl px-4 mx-auto " + (className && className)}>
+    <div className={"max-w-7xl px-4 mx-auto " + (className && className)}>
       {children}
     </div>
   );
