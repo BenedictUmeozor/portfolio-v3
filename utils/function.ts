da08c313d6f95efe94534d6742305285358d9c68
Benedict <benedictumeozor@gmail.com>
2024-03-15 11:33:47 +0100

    resolved window not defined error

--- utils/function.ts
+++ utils/function.ts
@@ -1,10 +1,4 @@
 export const getTheme = () => {
-  let theme =
-    window &&
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "dark";
-  theme = JSON.parse(localStorage.getItem("theme")!) ?? theme;
+  const theme = JSON.parse(localStorage.getItem("theme")!) ?? "dark";
   return theme;
 };
